c6c2c3e4d9a8509ad44c839dcf5d21040f037d37
chhagan123 <chhaganrakhade7@gmail.com>
2025-08-26 04:00:19 +0530

    style : change style in Edittask model

--- Frontend/src/components/EditTaskModal.jsx
+++ Frontend/src/components/EditTaskModal.jsx
@@ -35,7 +35,7 @@ function EditTaskModal({ task, onSave, onClose }) {
   if (!task) return null; // don't render if no task selected
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center text-black bg-black/50 bg-blur">
+    <div className="fixed inset-0 flex items-center justify-center text-black bg-black/40 backdrop-blur-sm bg-opacity-30">
       <form
         onSubmit={handleSubmit}
         className="bg-white p-5 rounded-md shadow-md w-80"
@@ -43,6 +43,7 @@ function EditTaskModal({ task, onSave, onClose }) {
         <h2 className="text-lg font-bold mb-3">Edit Task</h2>
 
         {/* Title */}
+        <label>Title</label>
         <input
           className="border w-full p-2 rounded mb-3"
           value={title}
@@ -51,7 +52,8 @@ function EditTaskModal({ task, onSave, onClose }) {
         />
 
         {/* Description */}
-        <input
+        <label>Description</label>
+        <textarea
           className="border w-full p-2 rounded mb-3"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
@@ -59,6 +61,7 @@ function EditTaskModal({ task, onSave, onClose }) {
         />
 
         {/* Assign Name */}
+        <label>User</label>
         <input
           className="border w-full p-2 rounded mb-3"
           value={assignName}
@@ -67,6 +70,7 @@ function EditTaskModal({ task, onSave, onClose }) {
         />
 
         {/* Due Date */}
+        <label>Due Date</label>
         <input
           type="date"
           className="border w-full p-2 rounded mb-3"
